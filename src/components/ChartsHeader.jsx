375a860bc2c93a0b2ded694c8bf1a905f12bd94e
mbohbless <mbohblesspearl@gmail.com>
2022-10-11 11:35:42 +0100

    set up completion

---
+++ src/components/ChartsHeader.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CharstHeader = () => {
+  return (
+    <div>ChartHeader</div>
+  )
+}
+
+export default CharstHeader
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SideBar = () => {
+  return (
+    <div>Sizebar</div>
+  )
+}
+
+export default SideBar
